f0f4c8665f6d94c517bcaa27f3545cc9ca52212d
Erik Schnetter <schnetter@gmail.com>
2024-08-29 12:32:39 -0400

    Disable partitioned communication tests

--- threaded/tst_threaded_ring_partitioned.c
+++ threaded/tst_threaded_ring_partitioned.c
@@ -25,6 +25,7 @@ static int ratio_send_to_receive = 1;
 
 int tst_threaded_ring_partitioned_init(struct tst_env *env)
 {
+#if 0
   int comm_rank;
   MPI_Comm comm = tst_comm_getmastercomm(env->comm);
   MPI_CHECK(MPI_Comm_rank(comm, &comm_rank));
@@ -67,6 +68,8 @@ int tst_threaded_ring_partitioned_init(struct tst_env *env)
     tst_type_setstandardarray(env->type, num_worker_threads * env->values_num, env->send_buffer, comm_rank);
 
   return 0;
+#endif
+  return 0;
 }
 
 
@@ -74,6 +77,7 @@ int tst_threaded_ring_partitioned_init(struct tst_env *env)
 // returns 1 if the partition has arrived and 0 if waiting was interupted
 static int wait_for_partition(MPI_Request *recv_request, int partition_num, useconds_t backoff_time)
 {
+#if 0
   int flag = 0;
   do
   {
@@ -81,10 +85,13 @@ static int wait_for_partition(MPI_Request *recv_request, int partition_num, usec
   } while (flag == 0 && usleep((backoff_time = (backoff_time * 3) / 2)) == 0);
 
   return flag;
+#endif
+  return 0;
 }
 
 int tst_threaded_ring_partitioned_run(struct tst_env *env)
 {
+#if 0
   int comm_size;
   int comm_rank;
   int send_to;
@@ -218,10 +225,13 @@ int tst_threaded_ring_partitioned_run(struct tst_env *env)
     return tst_test_checkstandardarray(env, env->recv_buffer, TST_RANK_MASTER);
   else
     return 0;
+#endif
+  return 0;
 }
 
 int tst_threaded_ring_partitioned_cleanup(struct tst_env *env)
 {
+#if 0
   int thread_num = tst_thread_get_num();
   int num_worker_threads = tst_thread_num_threads();
 
@@ -234,5 +244,7 @@ int tst_threaded_ring_partitioned_cleanup(struct tst_env *env)
     pthread_barrier_destroy(&thread_barrier);
   }
 
+  return 0;
+#endif
   return 0;
 }
